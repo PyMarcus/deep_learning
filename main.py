000290e5dc46d081dbba91b150b91fccc96e6ae8
Marcus <88283829+PyMarcus@users.noreply.github.com>
2022-11-12 17:44:43 -0300

    analisa e decifra vegetais

---
+++ main.py
@@ -0,0 +1,146 @@
+import sklearn
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from pathlib import Path
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.layers.experimental.preprocessing import RandomFlip
+from tensorflow.keras.layers.experimental.preprocessing import RandomRotation
+from tensorflow.keras.layers.experimental.preprocessing import RandomZoom
+from tensorflow.keras.applications import EfficientNetB3
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.metrics import Precision
+from tensorflow.keras.metrics import Recall
+tf.random.set_seed(4)
+
+
+def carrega_transforma(image, label):
+    """ Função para carregar e transformar as imagens"""
+    image = tf.io.read_file(image)
+    image = tf.io.decode_jpeg(image, channels = 3)  # canal = rgb
+    return image, label
+
+
+def prepara_dataset(path, labels, train=True):
+    global autotune, resize, batch_size, data_augmentation
+    """Função para preparar os dados noo formato do TensorFlow"""
+    # Prepara os dados
+    image_paths = tf.convert_to_tensor(path)
+    labels = tf.convert_to_tensor(labels)
+    image_dataset = tf.data.Dataset.from_tensor_slices(image_paths)
+    label_dataset = tf.data.Dataset.from_tensor_slices(labels)
+    dataset = tf.data.Dataset.zip((image_dataset, label_dataset))
+    dataset = dataset.map(lambda image, label: carrega_transforma(image, label))
+    dataset = dataset.map(lambda image, label: (resize(image), label), num_parallel_calls=autotune)
+    dataset = dataset.shuffle(1000)
+    dataset = dataset.batch(batch_size)
+    # Se train = True aplica dataset augmentation
+    if train:
+        dataset = dataset.map(lambda image, label: (data_augmentation(image), label), num_parallel_calls=autotune)
+    # Se train = False repete sobre o dataset e retorna
+    dataset = dataset.repeat()
+    return dataset
+
+
+def extrai_label(caminho_imagem):
+    return caminho_imagem.split("/")[-2]
+
+
+def ver_nome_da_image():
+    print(encoder.inverse_transform(np.argmax(label, axis=1))[0])  # inverte de números para imagem
+    plt.imshow((imagem[0].numpy()/255).reshape(224, 224, 3))
+
+
+def prepara_dataset_validacao():
+    dataset_valid = prepara_dataset(X_valid, y_valid, train=False)
+    imagem, label = next(iter(dataset_valid))
+    return dataset_valid
+
+
+def cria_modelo():
+    modelo = EfficientNetB3(input_shape=(224,224,3), include_top=False)  # remove head do modelo
+    #add camadas ao modelo pré (acrescenta no lugar da head)
+    modelo = tf.keras.Sequential([
+        modelo, tf.keras.layers.GlobalAveragePooling2D(), tf.keras.layers.Dense(131, activation="softmax")
+    ])
+    # sumário do modelo
+    modelo.summary()
+    # hyperparametros:
+    lr = 0.001
+    beta1 = 0.9
+    beta2 = 0.999
+    ep = 1e-7
+
+    # compilacao do modelo:
+    modelo.compile(optimizer=Adam(learning_rate=lr,  # adam é a funcao q realiza backpropagation
+                                  beta_1=beta1,
+                                  beta_2=beta2,
+                                  epsilon=ep),
+                   loss="categorical_crossentropy",
+                   )
+    # treinar(modelo)
+    """
+    modelo.layers[0].trainable = False
+    checkpoint = tf.keras.callbacks.ModelCheckpoint("model/melhor_modelo.h5",  # quando encontrar o melhor modelo, salva
+                                                    verbose=1,
+                                                    save_best=True,
+                                                    save_weights_only=True)
+    early_stop = tf.keras.callbacks.EarlyStopping(patience=4)
+    modelo.summary()
+    modelo.layers[0].trainable = True
+    """
+    # Carrega os pesos do ponto de verificação e reavalie
+    modelo.load_weights("model/melhor_modelo.h5")
+
+
+
+def treinar(modelo):
+    """
+    [PODE LEVAR VÁRIAS HORAS]
+    Na cpu, as operações são sequenciais, ou seja, mais lentas
+    Se encontrar uma gpu, treina nel"""
+    history = modelo.fit(dataset_treino,
+                         steps_per_epoch=len(X_treino)//batch_size,
+                         epochs=1,
+                         validation_data=prepara_dataset_validacao(),
+                         validation_steps=len(y_treino)//batch_size)
+
+
+
+if __name__ == '__main__':
+    diretorio_atual = Path.cwd()
+    print(diretorio_atual)
+    # define caminho para os dados de entrada
+    caminho_dados_treino = Path("fruits-360/Training")
+    caminho_dados_teste = Path("fruits-360/Test")
+    imagens_treino = list(caminho_dados_treino.glob("*/*"))
+    # com funções anônimas, é aplicado, a cada item, a função
+    imagens_treino = list(map(lambda x: str(x), imagens_treino))
+    imagens_treino_labels = list(map(lambda x: extrai_label(x), imagens_treino))
+    # converte texto para números para reconhecimento
+    encoder = LabelEncoder()
+    imagens_treino_labels = encoder.fit_transform(imagens_treino_labels)
+
+    imagens_treino_labels = tf.keras.utils.to_categorical(imagens_treino_labels)
+    X_treino, X_valid, y_treino, y_valid = train_test_split(imagens_treino, imagens_treino_labels)
+    # Redimensionamento de todas as imagens para 224 x 224
+    img_size = 224
+    resize = tf.keras.Sequential([tf.keras.layers.experimental.preprocessing.Resizing(img_size, img_size)])
+    # Cria o objeto para dataset augmentation
+    data_augmentation = tf.keras.Sequential([RandomFlip("horizontal"),
+                                             RandomRotation(0.2),
+                                             RandomZoom(height_factor=(-0.3, -0.2))])
+    # Hiperparâmnetros
+    batch_size = 32  # limita a quantidade de imagens, do contrário, estoura memória
+    autotune = tf.data.experimental.AUTOTUNE
+    # Cria o dataset de treino
+    dataset_treino = prepara_dataset(X_treino, y_treino)
+
+    # Shape
+    imagem, label = next(iter(dataset_treino))  # 32 imagens, conforme o lot de batch acima
+    print(imagem.shape)  # tamanho, pixel larg, pixel alt, canal de cor (RGB)
+    print(label.shape)   # 32 labels, 131 = tamanho dos itens na pasta
+    ver_nome_da_image()
+    prepara_dataset_validacao()
+    cria_modelo()
